af7df1e6856c50119e92e133359e064d7a878c16
bon ho <82163197+junhyeonsin@users.noreply.github.com>
2022-08-25 17:52:16 +0900

    Update skill.py

--- renwal/skill.py
+++ renwal/skill.py
@@ -74,7 +74,7 @@ class skillModify():
     else:
       return value
   def upgrade(self,id,level,point):
-    cur.execute(f"UPDATE user_stat SET skill_point=skill_point-{point} WHERE id = %s ",(self.id))
+    cur.execute(f"UPDATE user_stat SET skill_point=skill_point-`{point}` WHERE id = %s ",(self.id))
     cur.execute(f"SELECT * FROM skill WHERE skill_id = %s AND skill_level = %s ",(id,level+1))
     con.commit()
     info=list(cur.fetchone())
