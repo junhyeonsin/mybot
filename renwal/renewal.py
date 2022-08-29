9ec16838af615f65fef1a06f8ffde7993ca92e14
bon ho <82163197+junhyeonsin@users.noreply.github.com>
2022-08-29 22:29:57 +0900

    Update renewal.py

--- renwal/renewal.py
+++ renwal/renewal.py
@@ -210,11 +210,8 @@ async def playmusic(interaction:Interaction,url_title:str,먼저틀기:bool=Fals
     else:
       queue[guild].append(player)
     if not voice_client.is_playing():
-      voice_client.play(player,after=nextsong(interaction))
-      embed=discord.Embed(title=player.title)
-      embed.set_image(url=player.from_url)
-      await interaction.edit_original_response(embed=embed)
-      #await interaction.edit_original_response(content=f"{player.title} 재생중!!")
+      voice_client.play(player,after=None)
+      await interaction.edit_original_response(content=f"{player.title} 재생중!!")
     else:
       await interaction.edit_original_response(content=f"{player.title} 재생목록 추가됨!")
     await asyncio.sleep(7)
