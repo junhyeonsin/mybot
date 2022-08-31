d879307a4fe9343204de7563b5aa71374cc69bbd
bon ho <82163197+junhyeonsin@users.noreply.github.com>
2022-08-31 21:42:27 +0900

    Update renewal_class.py

--- renwal/renewal_class.py
+++ renwal/renewal_class.py
@@ -207,7 +207,7 @@ class MakeItem():
           return True
     return False
   def etc(self,id,name):
-    cur.execute(f"SELECT * FROM make_etc WHERE item_name = %s ",name)
+    cur.execute(f"SELECT * FROM make_etc WHERE item_name = %s",name)
     make_item=cur.fetchone()
     if make_item[4]:
       need_etc=make_item[4].split(" ")
