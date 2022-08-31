c41bac48b2a50b3dd43d7adf4fc409894806d40e
bon ho <82163197+junhyeonsin@users.noreply.github.com>
2022-08-31 19:55:37 +0900

    Update renewal_class.py

--- renwal/renewal_class.py
+++ renwal/renewal_class.py
@@ -159,9 +159,11 @@ class MakeItem():
         cur.execute(f"SELECT item_amount FROM `{id}_use` WHERE item_code={i}")
         use_amount.append(cur.fetchone()[0])
     return need_etc,need_etc_amount,etc_amount,need_use,need_use_amount,use_amount
+  def disable(self,name,id,data):
+    self.callamount(name,id,data)
 
   def etc(self,id,name):
-    cur.execute(f"SELECT * FROM make_etc WHERE item_name = {name} ")
+    cur.execute(f"SELECT * FROM make_etc WHERE item_name = `{name}` ")
     make_item=cur.fetchone()
     if make_item[4]:
       need_etc=make_item[4].split(" ")
