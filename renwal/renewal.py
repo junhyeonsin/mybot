67ded7bbd2e69238e534ef429e12b2d6ec3bb94d
bon ho <82163197+junhyeonsin@users.noreply.github.com>
2022-10-11 12:45:32 +0900

    Update renewal.py

--- renwal/renewal.py
+++ renwal/renewal.py
@@ -43,7 +43,7 @@ class MyClient(discord.Client):
       emoji_id=emoji_id.replace(">","")
       cur=con.cursor()
       #cur.execute("CREATE TABLE IF NOT EXISTS onoff(GUILD BIGINT PRIMARY KEY,ONOFF BOOL)")
-      cur.execute("SELECT ONOFF FROM onoff WHERE GUILD = %s",(guild.id))
+      cur.execute("SELECT ONOFF FROM onoff WHERE GUILD = %s",guild.id)
       check=cur.fetchone()
       if not check:
         cur.execute("INSERT INTO onoff VALUES(%s,%s)",(guild.id,True))
